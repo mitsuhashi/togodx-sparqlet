2360758967819fc33d1e5308dd0c630f23eb3dbc
Nobutaka Mitsuhashi <mitsuhashi@dbcls.rois.ac.jp>
2023-03-21 23:25:27 +0900

    auto commit

---
+++ sync_with_github.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function check_previous_sync_alive(){
+  local script_name=$(basename $0)
+  local sync_processes=$(ps -ef | grep $script_name) 
+  local num_sync_processes=$(ps -ef | grep $script_name | wc -l) 
+
+  if [ $num_sync_processes -ge 4 ]; then
+    echo "true" 
+  else
+    echo "false"
+  fi
+}   
+
+is_previous_sync_alive=`check_previous_sync_alive`
+
+if [ "$is_previous_sync_alive" == "false" ] ; then
+  exit 0
+fi
+
+git add -A .
+git commit -m 'auto commit'
+git pull --rebase
+git push
